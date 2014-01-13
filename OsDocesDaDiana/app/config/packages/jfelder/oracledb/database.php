2f7c50c99e4f9856f2bdc3919b55029ac111fe0f
Miguel Pinto <mcpinto98@gmail.com>
2014-01-13 15:10:25 +0000

    possível configuração BD para laravel

--- OsDocesDaDiana/app/config/packages/jfelder/oracledb/database.php
+++ OsDocesDaDiana/app/config/packages/jfelder/oracledb/database.php
@@ -6,10 +6,10 @@ return array(
 		'oracle' => array(
 			'driver'   => 'oci',
 			'host'     => 'localhost',
-                        'port'     => '1521',
-			'database' => 'database',
-			'username' => 'root',
-			'password' => '',
+            'port'     => '1521',
+			'database' => 'OsDoceDaDiana',
+			'username' => 'ODDD',
+			'password' => 'ODDD',
 			'prefix'   => '',
 		),
 
