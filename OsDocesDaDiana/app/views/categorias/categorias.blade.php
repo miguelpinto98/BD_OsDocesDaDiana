59883d6b11e1fcbf9d3bc95480f951e869205cea
Inês Medeiros <mari.ines.medeiros7@gmail.com>
2014-01-24 03:37:09 +0000

    categorasView - not sure

---
+++ OsDocesDaDiana/app/views/categorias/categorias.blade.php
@@ -0,0 +1,169 @@
+@extends('layout')
+
+@include('headerbar')
+
+@section('content')
+	<div class="center">
+		<div id="news-box" class="content-box">
+			<div id="news-list" class="news-list">
+				
+
+				<div class="clear"></div>
+					<div id="new55" class="item first">
+						<!--Primeiro Quadrado-->
+							<div class="new-imagecategorias">
+								<a href="categoria.html">
+									<img src="assets/images/categorias/docesdecolher.png" width="217px" height="217px"/>
+								</a>
+								<div class="title-bg">
+									<div class="title"><a href="categoria.html">Doces de Colher</a></div>
+								</div>
+							</div>
+						<!--Primeiro Quadrado-->
+
+
+						<!--Segundo Quadrado-->
+							<div class="new-imagecategorias">
+								<a href="categoria.html">
+									<img src="assets/images/categorias/cupcakes.png" width="217px" height="217px"/>
+								</a>
+									<div class="title-bg">
+										<div class="title"><a href="categoria.html">Bolinhos</a></div>
+									</div>
+							</div>
+						<!--Segundo Quadrado-->
+
+
+						<!--Terceiro Quadrado-->
+							<div class="new-imagecategorias">
+								<a href="categoria.html">
+									<img src="assets/images/categorias/bolos.png" width="217px" height="217px"/>
+								</a>
+									<div class="title-bg">
+										<div class="title"><a href="categoria.html">Bolos</a></div>
+									</div>
+							</div>
+						<!--Terceiro Quadrado-->
+
+						<!--Quarto Quadrado-->
+							<div class="new-imagecategorias">
+								<a href="categoria.html">
+									<img src="assets/images/categorias/bolachas.png" width="217px" height="217px"/>
+								</a>
+									<div class="title-bg">
+										<div class="title"><a href="categoria.html">Bolachas e Biscoitos</a></div>
+									</div>
+							</div>
+						<!-- Quarto Quadrado-->
+					</div>
+
+
+
+				<div class="clear"></div>
+					<div id="new50" class="item">
+				
+						<!--Quinto Quadrado-->
+							<div class="new-imagecategorias">
+								<a href="categoria.html">
+									<img src="assets/images/categorias/tartes.png" width="217px" height="217px"/>
+								</a>
+									<div class="title-bg">
+										<div class="title"><a href="categoria.html">Tartes</a></div>
+									</div>
+							</div>
+						<!--Quinto Quadrado-->
+
+
+						<!--Sexto Quadrado-->
+							<div class="new-imagecategorias">
+								<a href="categoria.html">
+									<img src="assets/images/categorias/torta.png" width="217px" height="217px"/>
+								</a>
+									<div class="title-bg">
+										<div class="title"><a href="categoria.html">Tortas</a></div>
+									</div>
+							</div>
+						<!--Sexto Quadrado-->
+
+						<!--Sétimo Quadrado-->
+							<div class="new-imagecategorias">
+								<a href="categoria.html">
+									<img src="assets/images/categorias/pudim.png" width="217px" height="217px"/>
+								</a>
+									<div class="title-bg">
+										<div class="title"><a href="categoria.html">Pudins</a></div>
+									</div>
+							</div>
+						<!--Sétimo Quadrado-->
+
+
+						<!--Oitavo Quadrado-->
+							<div class="new-imagecategorias">
+								<a href="categoria.html">
+									<img src="assets/images/categorias/bombons.png" width="217px" height="217px"/>
+								</a>
+									<div class="title-bg">
+										<div class="title"><a href="categoria.html">Bombons e Docinhos</a></div>
+									</div>
+							</div>
+						<!--Oitavo Quadrado-->
+					</div>
+
+
+
+				<div class="clear"></div>
+					<div id="new50" class="item">
+						
+
+
+						<!--Nono Quadrado-->
+							<div class="new-imagecategorias">
+								<a href="#">
+									<img src="assets/images/categorias/semi-frio.png" width="217px" height="217px"/>
+								</a>
+									<div class="title-bg">
+										<div class="title"><a href="categoria.html">Semi-frios</a></div>
+									</div>
+							</div>
+						<!--Nono Quadrado-->
+
+						<!--Décimo Quadrado-->
+							<div class="new-imagecategorias">
+								<a href="#">
+									<img src="assets/images/categorias/gelado.png" width="217px" height="217px"/>
+								</a>
+									<div class="title-bg">
+										<div class="title"><a href="categoria.html">Gelados</a></div>
+									</div>
+							</div>
+						<!--Décimo Quadrado-->
+
+
+						<!--Décimo-primeiro Quadrado-->
+							<div class="new-imagecategorias">
+								<a href="#">
+									<img src="assets/images/categorias/frito2.png" width="217px" height="217px"/>
+								</a>
+									<div class="title-bg">
+										<div class="title"><a href="categoria.html">Doces Fritos</a></div>
+									</div>
+							</div>
+						<!--Décimo-primeiro Quadrado-->
+
+
+						<!--Décimo-segundo Quadrado-->
+							<div class="new-imagecategorias">
+								<a href="#">
+									<img src="assets/images/categorias/fruto.png" width="217px" height="217px"/>
+								</a>
+									<div class="title-bg">
+										<div class="title"><a href="categoria.html">Frutos e Outros</a></div>
+									</div>
+							</div>
+						<!--Décimo-segundo Quadrado-->
+					</div>
+
+			</div>
+		</div>
+	</div>
+@stop
