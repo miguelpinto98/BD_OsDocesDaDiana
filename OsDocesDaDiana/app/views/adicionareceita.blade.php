0fd2de87563629a510cd9e7ec36715c3959bffa1
Inês Medeiros <mari.ines.medeiros7@gmail.com>
2014-01-26 05:59:02 +0000

    jbn

--- OsDocesDaDiana/app/views/adicionareceita.blade.php
+++ OsDocesDaDiana/app/views/adicionareceita.blade.php
@@ -29,34 +29,52 @@
 									<div class="post-comment" style="height:770px";>
 
 															
+										
 										<div class="post-comment-box"style="height:30px">
 											<span>Categoria</span>
-											<textarea id="categoria" style="margin-left:160px; height:20px; width:120px" name="categoria" value="" onblur="textareaLabel(0);" onfocus="textareaLabel(1);"></textarea>
+											<select class="dropdown" style="margin-left:155px; height:20px; width:200px">
+												<option value="" class="label">Seleccione</option>
+												<option value="1">Doces de Colher</option>
+												<option value="2">Bolinhos</option>
+												<option value="3">Bolos</option>
+												<option value="4">Bolachas e Biscoitos</option>
+												<option value="5">Tartes</option>
+												<option value="6">Tortas</option>
+												<option value="7">Pudins</option>
+												<option value="8">Bombons e Docinhos</option>
+												<option value="9">Semi-Frios</option>
+												<option value="10">Gelados</option>
+												<option value="11">Doces Fritos</option>
+												<option value="12">Frutos e Outros</option>
+												<option value=""></option>
+											</select>
 										</div>
+
+
 										<div class="main-separator1"></div>	
 
 
 										<div class="post-comment-box"style="height:30px">
 											<span>Nome</span>
-											<textarea id="nome" style="margin-left:185px; height:20px; width:120px" name="nome" value="" onblur="textareaLabel(0);" onfocus="textareaLabel(1);"></textarea>
+											<textarea id="nome" style="margin-left:185px; height:20px; width:200px" name="nome" value="" onblur="textareaLabel(0);" onfocus="textareaLabel(1);"></textarea>
 										</div>
 										<div class="main-separator1"></div>
 
 										<div class="post-comment-box" style="height:30px">
 											<span>Tempo de Preparação</span>
-											<textarea id="tempo" style="margin-left:80px; height:20px; width:120px" name="tempo" value="" onblur="textareaLabel(0);" onfocus="textareaLabel(1);"></textarea>
+											<textarea id="tempo" style="margin-left:80px; height:20px; width:200px" name="tempo" value="" onblur="textareaLabel(0);" onfocus="textareaLabel(1);"></textarea>
 										</div>
 										<div class="main-separator1"></div>
 
 										<div class="post-comment-box" style="height:30px">
 											<span>Dose (número de pessoas)</span>
-											<textarea id="DOSE" style="margin-left:50px; height:20px; width:120px" name="dose" value="" onblur="textareaLabel(0);" onfocus="textareaLabel(1);"></textarea>
+											<textarea id="DOSE" style="margin-left:50px; height:20px; width:200px" name="dose" value="" onblur="textareaLabel(0);" onfocus="textareaLabel(1);"></textarea>
 										</div>
 										<div class="main-separator1"></div>
 
 										<div class="post-comment-box" style="height:30px">
 											<span>Custo (em €/por dose)</span>
-											<textarea id="custo" style="margin-left:80px; height:20px; width:120px" name="custo" value="" onblur="textareaLabel(0);" onfocus="textareaLabel(1);"></textarea>
+											<textarea id="custo" style="margin-left:80px; height:20px; width:200px" name="custo" value="" onblur="textareaLabel(0);" onfocus="textareaLabel(1);"></textarea>
 										</div>
 										<div class="main-separator1"></div>
 
