f187fbae76cb9c6452a19a6db71d8f33f8b2531a
Inês Medeiros <mari.ines.medeiros7@gmail.com>
2014-01-26 18:42:10 +0000

    m

--- OsDocesDaDiana/app/controllers/AdicionaController.php
+++ OsDocesDaDiana/app/controllers/AdicionaController.php
@@ -20,7 +20,7 @@ class AdicionaController extends BaseController {
         foreach($_FILES['file'] as $key => $abc) {
             $temp = explode(".", $_FILES["file"]["name"]);
             $extension = end($temp);
-            $destinationPath = 'assets/images/uploads/'.str_random(8).'.'.$extension;
+            $destinationPath = 'assets/images/uploads/'."cena".'.'.$extension;
             var_dump($_FILES); exit();
             if(in_array($extension, $allowedExts) && ($_FILES["file"]["size"] < 20000000)) {
                 if($_FILES["file"]["error"] > 0) {
@@ -28,7 +28,7 @@ class AdicionaController extends BaseController {
                 }
                 $uploadSuccess=move_uploaded_file($_FILES["file"]["tmp_name"],$destinationPath);
                 if( $uploadSuccess ) {
-                    return Redirect::to('categorias');
+                    return Redirect::to('adicion');
                 } else {
                     return Response::json('error_else', 400);
                 }
