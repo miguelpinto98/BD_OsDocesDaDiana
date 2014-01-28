ffa981fc9a8018335f7b8f05d75224b4f0e4a150
Inês Medeiros <mari.ines.medeiros7@gmail.com>
2014-01-28 21:23:29 +0000

    ´x

--- OsDocesDaDiana/app/views/footer.blade.php
+++
@@ -1,8 +0,0 @@
-@section('footer')
-	<div class="center">
-		<div id="footer">
-			Desenvolvido por Diana Lemos, Mariana Medeiros, Mário Leite, Miguel Pinto e Pedro Duarte.
-		</div>
-	</div>
-
-@stop
