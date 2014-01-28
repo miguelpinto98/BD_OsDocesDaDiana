a40a7334b919cceca853c4afdcf4c294bba2db02
Miguel Pinto <mcpinto98@gmail.com>
2014-01-28 02:22:00 +0000

    mais cenas

--- BackEnd/ODDD/src/GUI/PainelAdmin.java
+++ BackEnd/ODDD/src/GUI/PainelAdmin.java
@@ -315,6 +315,11 @@ public final class PainelAdmin extends javax.swing.JFrame {
         jPanel5.setLayout(new java.awt.BorderLayout());
 
         jButton1.setText("Adicionar");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
 
         listaUser.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
@@ -427,6 +432,11 @@ public final class PainelAdmin extends javax.swing.JFrame {
             this.jPanel5.validate();
         }
     }//GEN-LAST:event_consultarUserActionPerformed
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handling code here:
+        
+    }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
      * @param args the command line arguments
